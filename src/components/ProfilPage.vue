cfab2d973a311a4f035a388329ab74efba1d0d43
nabil <nabil-13130@hotmail.fr>
2022-10-14 09:23:49 +0200

    add list

--- src/components/ProfilPage.vue
+++ src/components/ProfilPage.vue
@@ -1,26 +1,88 @@
 <script>
-export default {
+  export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
 
-    data() {
-            return {
-                newTask: "",
-                taskList: [],
-            };
+  data() {
+    return {
+      task: "",
+      editedTask: null,
+      statuses: ["to-do", "in-progress", "finished"],
+
+      /* Status could be: 'to-do' / 'in-progress' / 'finished' */
+      tasks: [
+        {
+          name: "Steal bananas from the supermarket.",
+          status: "to-do",
         },
-        methods: {
-            addTask: function () {
-                if (this.newTask === "") {
-                    return;
-                }
-                const newTask = {
-                    text: this.newTask,
-                    status: "todo",
-                };
-                this.taskList.push(newTask);
-                this.newTask = "";
-            }
+        {
+          name: "Eat 1 kg chocolate in 1 hour.",
+          status: "in-progress",
         },
-}
+        {
+          name: "Create YouTube video.",
+          status: "finished",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    /**
+     * Capitalize first character
+     */
+    capitalizeFirstChar(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    },
+
+    /**
+     * Change status of task by index
+     */
+    changeStatus(index) {
+      let newIndex = this.statuses.indexOf(this.tasks[index].status);
+      if (++newIndex > 2) newIndex = 0;
+      this.tasks[index].status = this.statuses[newIndex];
+    },
+
+    /**
+     * Deletes task by index
+     */
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+
+    /**
+     * Edit task
+     */
+    editTask(index) {
+      this.task = this.tasks[index].name;
+      this.editedTask = index;
+    },
+
+    /**
+     * Add / Update task
+     */
+    submitTask() {
+      if (this.task.length === 0) return;
+
+      /* We need to update the task */
+      if (this.editedTask != null) {
+        this.tasks[this.editedTask].name = this.task;
+        this.editedTask = null;
+      } else {
+        /* We need to add new task */
+        this.tasks.push({
+          name: this.task,
+          status: "todo",
+        });
+      }
+
+      this.task = "";
+    },
+  },
+};
 </script>
 
 
@@ -35,9 +97,48 @@ export default {
         </div>
         <div class="mini-titre">
         </div>
-        <ul>
-            {{newTask}}
-        </ul>
+        <table class="table table-bordered mt-5">
+      <thead>
+        <tr>
+          <th scope="col">Task</th>
+          <!-- <th scope="col" style="width: 120px">Status</th>
+          <th scope="col" class="text-center">#</th>
+          <th scope="col" class="text-center">#</th> -->
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(task, index) in tasks" :key="index">
+          <td>
+            <span :class="{ 'line-through': task.status === 'finished' }">
+              {{ task.name }}
+            </span>
+          </td>
+          <td>
+            <span
+              class="pointer noselect"
+              @click="changeStatus(index)"
+              :class="{
+                'text-danger': task.status === 'to-do',
+                'text-success': task.status === 'finished',
+                'text-warning': task.status === 'in-progress',
+              }"
+            >
+              {{ capitalizeFirstChar(task.status) }}
+            </span>
+          </td>
+          <td class="text-center">
+            <div @click="deleteTask(index)">
+              <span class="fa fa-trash pointer"></span>
+            </div>
+          </td>
+          <td class="text-center">
+            <div @click="editTask(index)">
+              <p class="fa fa-pen pointer"></p>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
     </div>
 </template>
 
@@ -69,4 +170,24 @@ ul{
 .mini-titre{
     text-align: center;
 }
+
+
+
+
+.pointer {
+  cursor: pointer;
+}
+.noselect {
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
+}
+.line-through {
+  text-decoration: line-through;
+}
+
 </style>
