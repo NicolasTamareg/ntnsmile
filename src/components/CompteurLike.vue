ce2bca3ad72824753d580daaf7828e4e61a5350f
nabil <nabil-13130@hotmail.fr>
2022-10-19 16:15:33 +0200

    DRAFFFFFFFT

--- src/components/CompteurLike.vue
+++ src/components/CompteurLike.vue
@@ -1,51 +1,82 @@
 <template>
-    <div class="numberLike">
-    <p>{{likeValue}}</p><button  @click="addLike" class="buttonjaimepost"><i class="fa-solid fa-heart"></i>
-          </button>
-    </div>
+  <div class="numberLike">
+    <p>{{likeValue}}</p><button @click="addLike" class="buttonjaimepost"><i class="fa-solid fa-heart"></i>
+    </button>
+  </div>
 </template>
 
 
 <script>
-export default{
-    data(){
-        return{
-            likeValue:0
-        }
-    },
-    methods: {
-            addLike(){
-                this.likeValue=this.likeValue + 1
-            }
+export default {
+  data() {
+    return {
+      likeValue: "",
+        
+      counternumber:[]
+      
+    };
+  },
+  methods: {
+    addLike() {
+      
+      
+      
+      
+
+
+      // const token = localStorage.getItem("token");
+      // if (token) {
+      //   const options = {
+      //     method: "POST",
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //       "Authorization": "bearer " + token,
+      //     },
+    
+      //     body: JSON.stringify({
+      //       postId: this.likeValue,
+      //     }),
+      //   };
+
+      //   console.log("like:",this.likeValue)
+      // }
+      //   const response = await fetch(
+      //     "https://social-network-api.osc-fr1.scalingo.io/ntmsmile/post",
+      //     options
+      //   );
+
+      //   const data = await response.json();
+      // }
     }
+  },
 }
 
 </script>
 
 <style scoped lang="scss">
-
-.numberLike{
-    display: flex;
+.numberLike {
+  display: flex;
 }
-.buttonjaimepost{
+
+.buttonjaimepost {
   border: none;
   margin-top: 5px;
   padding: 6px;
-    border-radius: 25px;
+  border-radius: 25px;
   background-color: white;
   cursor: pointer;
 
-  i{
+  i {
     border-radius: 30px;
     background-color: #1DA1F2;
     padding: 10px;
     color: white;
 
-  &:hover{
+    &:hover {
 
-    background-color:white ;
-    color:  #1DA1F2;
-    transition: 0.3s;
+      background-color: white;
+      color: #1DA1F2;
+      transition: 0.3s;
     }
   }
 }
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,35 +1,47 @@
 <script >
-import CompteurLike from './CompteurLike.vue';
+
 
 export default {
   data() {
     return {
       newPost: "",
-      allPost: [],
-      numberLike: [],
-      affichePost:{
+      newComment: "",
+      allPost: [
 
-        
-      },
+
+      ],
+
+
+      shouldDisplayComm: false,
 
     };
 
+
+
   },
   methods: {
-    // Récupère la valeur contenue dans l'input et l'assigne à newTask
+    //affiche la div formulaire
+    afficheComm: function () {
+      this.shouldDisplayComm = true;
+    },
+    setNewComm: function (event) {
+      this.newComment = event.target.value
+    },
+    // Récupère la valeur contenue dans l'input et l'assigne à newPost
     setNewPost: function (event) {
       this.newPost = event.target.value;
-      console.log(this.newPost);
+
     },
     addToPost: async function () {
       console.log(this.allPost);
+
       const token = localStorage.getItem("token");
       if (token) {
         const options = {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
-            "Authorization": "bearer " + token,
+            Authorization: "bearer " + token,
           },
           body: JSON.stringify({
             title: this.newPost,
@@ -44,42 +56,123 @@ export default {
 
         const data = await response.json();
         if (data.success) {
-          this.allPost.push(this.newPost);
+          await this.getPosts();
           this.newPost = "";
+        } else {
+          alert("Veuillez vous inscrire");
+        }
+
+        console.log(this.allPost);
+      }
+    },
+
+    addToComm: async function (reccupid) {
+      const token = localStorage.getItem("token");
+      if (token) {
+        const options = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": "bearer " + token,
+          },
+          body: JSON.stringify({
+            postId: `${reccupid}`,
+            content: this.newComment,
+          }),
+        };
+
+        const response = await fetch(
+          "https://social-network-api.osc-fr1.scalingo.io/ntmsmile/post/comment",
+          options
+        );
+
+        const data = await response.json();
+        console.log("ladata", data)
+        if (data.success) {
+          await this.getPosts();
+          this.newComment = "";
+          this.shouldDisplayComm = false
         } else {
           alert("Veuillez vous inscrire")
         }
-        console.log(this.allPost);
 
+
+
+      }
+    },
+
+    getPosts: async function () {
+      const options = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      const serveurReponse = await fetch(
+        "https://social-network-api.osc-fr1.scalingo.io/ntmsmile/posts?page=0&limit=20",
+        options
+      );
+
+      const postAffich = await serveurReponse.json();
+      this.allPost = postAffich.posts;
+      // for (var i = 0; i < postAffich.posts.length; i++) {// permet de relier le i a l'index et .list.lenght pour la longueur du tableau
+      // this.afficheLike.id=postAffich.posts[i]._id
+      // }
+
+      console.log(this.allPost)
+
+    },
+
+
+    getLikes: async function (reccupid) {
+
+      //post like 
+
+      // for (var i = 0; i < this.allPost.length; i++) {// permet de relier le i a l'index et .list.lenght pour la longueur du tableau
+      //       // this.allPost.posts[i]._id// ajoute toute les liste dans le tableau
+      //     }
+      //     console.log("ttrt",this.allPost)
+
+      //  console.log("pour le poste id",this.numberLike);
+
+      const token = localStorage.getItem("token");
+
+      if (token) {
+        const options = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": "bearer " + token,
+          },
+
+          body: JSON.stringify({
+            postId: `${reccupid}`,
+          }),
+
+        };
+        const ReponseLike = await fetch(
+          "https://social-network-api.osc-fr1.scalingo.io/ntmsmile/post/like",
+          options
+        );
+        const likeAffiche = await ReponseLike.json();
+        console.log(likeAffiche)
+        if (likeAffiche.success) {
+          await this.getPosts();
+
+        } else {
+
+        }
       }
     },
 
 
   },
-  
-  components: { CompteurLike },
 
-  mounted: async function makeApromise() {
-    const options = {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-           
-        },
-      };
-      const serveurReponse = await fetch(
-          "https://social-network-api.osc-fr1.scalingo.io/ntmsmile/posts?page=0&limit=20",
-          options
-        );
-
-        const postAffich = await serveurReponse.json();
-        console.log(postAffich);
+  mounted: function () {
+    this.getPosts();
 
   }
-
-}
-
-
+};
 </script>
 
 <template>
@@ -91,34 +184,47 @@ export default {
       <label>
         Qu'est-ce qu'on fait?
       </label>
-      <input :value="newPost" @input="setNewPost" type="text" name="task" id="inputPost" placeholder="Quoi de neuf?" />
+      <input @keyup.enter="addToPost" :value="newPost" @input="setNewPost" type="text" name="task" id="inputPost"
+        placeholder="Quoi de neuf?" />
       <button class="validpost" @click="addToPost" type="button">Poster</button>
-
     </div>
 
     <div class="affichePost">
       <ul class="">
         <li v-for="(post,index) in allPost" class="liPost">
-          <p>Nom de l'utisateur</p>
-          <p class="inputaffichepost">{{post}}</p>
+          <p>Fait par :{{post.firstname}}</p>
+          <p class="inputaffichepost">{{post.title}}</p>
 
           <div class="modiflipost">
-            <button class="buttonmodif" type="button">Commenter</button>
-            <CompteurLike />
+
+            <div class="numberLike">
+              <p>{{post.likes.length}}</p><button @click="getLikes(post._id)" class="buttonjaimepost"><i
+                  class="fa-solid fa-heart"></i>
+              </button>
+            </div>
+
+            <button class="buttonmodif" @click="afficheComm" type="button">Commenter</button>
+
+          </div>
+          <div class="ajoutcomment" v-if="shouldDisplayComm">
+            <input @input="setNewComm" :value="newComment" type="text" name="" id="inputcommentaire"
+              placeholder="Ajouter un commentaire">
+            <button @click="addToComm(post._id)" class="buttonajoutcom"><i class="fa-solid fa-envelope"></i></button>
+          </div>
+          <div v-for="(comment) in post.comments" class="affichecomment">
+            <p class="namepost">Commentaire ajouter par :
+            <p class="nameuser">{{comment.firstname}}</p>
+            </p>
+            <p class="inputaffichepost">{{comment.content}}</p>
 
           </div>
         </li>
       </ul>
 
 
+
     </div>
   </div>
-
-
-
-
-
-
 </template>
 
 <style scoped lang="scss">
@@ -132,10 +238,9 @@ label {
   text-align: center;
 }
 
-
 .modiflipost {
   margin-top: -13px;
-
+  display: flex;
 
 
 }
@@ -144,7 +249,6 @@ label {
   display: flex;
   justify-content: center;
   border: none;
-
 }
 
 #inputPost {
@@ -154,7 +258,6 @@ label {
   resize: none;
   background-color: rgb(232, 237, 236);
   padding-bottom: 40px;
-
 }
 
 .validpost {
@@ -166,16 +269,14 @@ label {
   margin-left: 300px;
   cursor: pointer;
   margin-top: 10px;
-  background-color: #1DA1F2;
+  background-color: #1da1f2;
   color: white;
 
   &:hover {
-
     background-color: white;
-    color: #1DA1F2;
+    color: #1da1f2;
     transition: 0.3s;
   }
-
 }
 
 .liPost {
@@ -186,8 +287,9 @@ label {
   border-radius: 20px;
 
   .inputaffichepost {
-    background: #d4d0d0;
+    background: #F1F0EF;
     padding: 30px;
+
     display: flex;
     font-size: smaller;
   }
@@ -198,22 +300,43 @@ label {
     text-align: center;
     cursor: pointer;
     margin-top: 10px;
-    background-color: #1DA1F2;
+    background-color: #1da1f2;
     color: white;
     padding: 10px;
     border-radius: 17px;
 
     &:hover {
-
       background-color: white;
-      color: #1DA1F2;
+      color: #1da1f2;
       transition: 0.3s;
     }
-
   }
+}
 
+.numberLike {
+  display: flex;
+}
 
+.buttonjaimepost {
+  border: none;
+  margin-top: 5px;
+  padding: 6px;
+  border-radius: 25px;
+  background-color: white;
+  cursor: pointer;
 
+  i {
+    border-radius: 30px;
+    background-color: #1da1f2;
+    padding: 10px;
+    color: white;
+
+    &:hover {
+      background-color: white;
+      color: #1da1f2;
+      transition: 0.3s;
+    }
+  }
 }
 
 .buttonjaimepost {
@@ -239,6 +362,60 @@ label {
   }
 }
 
+.ajoutcomment {
+  display: flex;
+
+  .buttonajoutcom {
+    border: none;
+    margin-top: 5px;
+    padding: 6px;
+    border-radius: 25px;
+    background-color: white;
+    cursor: pointer;
+
+    i {
+      border-radius: 30px;
+      background-color: #1DA1F2;
+      padding: 10px;
+      color: white;
+
+      &:hover {
+
+        background-color: white;
+        color: #1DA1F2;
+        transition: 0.3s;
+      }
+    }
+
+  }
+}
+
+.affichecomment {
+  border: 0.5px solid black;
+  border-radius: 10px;
+  margin-top: 15px;
+}
+
+.namepost {
+  display: flex;
+  align-items: center;
+}
+
+.nameuser {
+  font-style: italic;
+}
+
+#inputcommentaire {
+  width: 90%;
+  margin-top: 5px;
+  margin-left: 7px;
+
+  outline: none;
+  border: 1px solid #1DA1F2;
+  resize: none;
+  border-radius: 5px;
+}
+
 .newsposte {
   display: flex;
   flex-direction: column;
@@ -250,6 +427,5 @@ label {
   textarea {
     outline: none;
   }
-
 }
 </style>
