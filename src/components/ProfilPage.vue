3ac9d80500c1766f81ff800a5bd097efedbb3264
nabil <nabil-13130@hotmail.fr>
2022-10-19 15:45:20 +0200

    draft ultime

--- src/components/ProfilPage.vue
+++ src/components/ProfilPage.vue
@@ -1,36 +1,22 @@
 <script>
 export default {
-    
-    data() {
-        return {
-            newTask: "",
-            taskList: [],
-            userName: 'Nabil Nabil'
-        };
-},
-methods: {
-      // Récupère la valeur contenue dans l'input et l'assigne à newTask
-      setNewTask: function (event) {
-        this.newTask = event.target.value;
-      },
-
-      addTaskToList: async function () {
-        // Si l'input est vide, on s'en va
-        if (!this.newTask) return;
-        // Si la nouvelle tâche existe déjà dans taskList, on s'en va
-        if (this.taskList.some((task) => task.text != this.newTask && task.text === this.newTask )) return;
-        // On crée un nouvel objet pour "transférer" this.newTask (string) vers newTask (dictionnaire)
-        const newTask = {
-          text: this.newTask,
-          // createdAt: new Date().getTime(),
-          // status: 'todo' // 'doing', 'done'
-        };
-  
-        this.taskList = this.taskList.concat(newTask);
-        console.log(this.taskList);
-        this.newTask = "";
-
-        const options = {
+  data() {
+    return {
+      newTask: "",
+      postList: [],
+      userName: [],
+      profile: {},
+      user: {
+        firstname: ""
+      }
+    };
+  },
+  methods: {
+    setNewTask: function (event) {
+      this.newTask = event.target.value;
+    },
+    getPost: async function () {
+      const options = {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
@@ -40,137 +26,140 @@ methods: {
         "https://social-network-api.osc-fr1.scalingo.io/ntmsmile/posts?page=0&limit=20",
         options
       );
-
       const postAffich = await serveurReponse.json();
-      this.taskList = postAffich.posts;
-      console.log(this.taskList);
-      
-      // function getPost (){
-      //   getPost = this.taskList[0].push(newTask)
-      // }
-
-      },
-
-      // C'est pour changer le status avec 2 arguments : 
-      // - la tâche dont le statut doit être modifié,
-      // - et le statut qu'on veut lui donner
-      // changeStatus: function (taskToModify, status) {
-        // Comme au dessus, on crée un nouvel objet pour conserver le texte et changer le status de la task passée en paramètre
-        // const newTask = {
-        //   text: taskToModify.text,
-        //   status,
-        // };
-  
-        // On transforme la taskList avec .map
-      //   this.taskList = this.taskList.map((taskItem) => {
-      //     // Si l'item en cours (taskItem) est le même que la tâche à modifier
-      //     if (taskItem.text === taskToModify.text) {
-      //       // alors on retourne notre nouvel objet
-      //       return newTask;
-      //     }
-      //     // sinon on retourne l'original
-      //     return taskItem;
-      //   });
-      // },
-      removeTask: function (taskToRemove) {
-        const index = this.taskList.findIndex(task => task.text === taskToRemove.text);
-        this.taskList = this.taskList.slice(0, index).concat(this.taskList.slice(index + 1));
-        
-        this.taskList = this.taskList.filter((task) => {
-          return task.text !== taskToRemove.text;
-        });
-      }
+      this.postList = postAffich.posts;
+      console.log(this.postList);
     },
-    
-    
+    async getUserProfile() {
+      let tokens = localStorage.getItem("token");
+      const options = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `bearer ${tokens}`,
+        },
+      };
 
-    
-  };
+      const response = await fetch(
+        "https://social-network-api.osc-fr1.scalingo.io/ntmsmile/user",
+        options
+      );
 
+      const data = await response.json();
+      console.log(data);
+      this.profile = data;
+    },
+    addTaskToList: async function () {
+      if (!this.newTask) return;
+      if (
+        this.postList.some(
+          (task) => task.text != this.newTask && task.text === this.newTask
+        )
+      )
+        return;
+      const newTask = {
+        text: this.newTask,
+       
+      };
+      this.postList = this.postList.concat(newTask);
+      console.log(this.postList);
+      this.newTask = "";
+    },
+    removePost: function (postToRemove) {
+      const index = this.postList.findIndex(
+        (post) => post._id === postToRemove._id
+      );
+      this.postList = this.postList
+        .slice(0, index)
+        .concat(this.postList.slice(index + 1));
+      this.postList = this.postList.filter((post) => {
+        return post._id !== postToRemove._id;
+      });
+    },
+  },
+  mounted: async function () {
+    await this.getUserProfile();
+    this.getPost();
+  },
+};
 </script>
 
-
 <template>
-    <div class="main-profil">
-        <div class="fond-ecran">
-            <img class="main-image" src="https://www.coursinfo.fr/wp-content/uploads/2016/04/facebook-logo.jpg" alt="">
-        </div>
-        <div class="photo-pseudo">
-            <img class="img-profil" src="https://picsum.photos/seed/picsum/200/200" alt="">
-            <h2 :value="userName"> {{userName}} </h2>
-        </div>
-        
-        <label>
-      Qu'est-ce qu'on fait ?
-      <input :value="newTask" @input="setNewTask" type="text" name="task" />
-      <button @click="addTaskToList" type="button">Ajouter</button>
-    </label>    
+  <div class="main-profil">
+    <div class="fond-ecran">
+      <img
+        class="main-image"
+        src="https://www.coursinfo.fr/wp-content/uploads/2016/04/facebook-logo.jpg"
+        alt=""
+      />
+    </div>
+    <div class="photo-pseudo">
+      <img
+        class="img-profil"
+        src="https://picsum.photos/seed/picsum/200/200"
+        alt=""
+      />
+      
+        <h2>
+          {{profile.firstname}} {{profile.lastname}}
+        </h2>
+    </div>
+
 
     <div class="columns">
-       <ul>
-        <li  v-for="task in taskList">
-          {{ task.firstname }} à publié : <br> {{task.content}}
-          <button @click="removeTask(task)" type="button">Supprimer</button>
-        </li>
-      </ul> 
-      <!-- <ul>
-        <li class="task-li" v-for="task in doing">
-          {{ task.text }}
-          <button @click="changeStatus(task, 'done')" type="button">Terminer</button>
-        </li>
-      </ul> 
       <ul>
-        <li  v-for="task in done">
-          {{ task.text }}
-          <button @click="removeTask(task)" type="button">Supprimer</button>
-        </li>
-      </ul> -->
+        <template v-for="post in postList">
+          <li v-if="post.userId == profile._id">
+            {{ post.firstname }} à publié : <br />
+            {{ post.content }}
+            <button @click="removePost(post)" type="button">Supprimer</button>
+
+          </li>
+        </template>
+      </ul>
+     
     </div>
-    </div>
+  </div>
 </template>
 
 <style>
-*{
-    font-family: sans-serif;
-    margin: 0;
+* {
+  font-family: sans-serif;
+  margin: 0;
 }
 
-.main-profil{
-    margin: auto;
-    justify-content: center;
-    height: 100%;
-    width: 80%;
+.main-profil {
+  margin: auto;
+  justify-content: center;
+  height: 100%;
+  width: 80%;
 }
 
-.fa{
-    font-size: 40px;
+.fa {
+  font-size: 40px;
 }
-.text-center{
-    padding: 20px;
+.text-center {
+  padding: 20px;
 }
-.firstLi{
-    display: flex;
-}
-
-.photo-pseudo{
-    display: flex;
-    align-items: flex-end;
-    margin-left: 10%;
-    transform: translateY(-60%);
+.firstLi {
+  display: flex;
 }
 
-ul{
-    text-align: center;
-}
-.img-profil{
-    display: flex;
-    flex-direction: column;
-    border-radius: 50%;
+.photo-pseudo {
+  display: flex;
+  align-items: flex-end;
+  margin-left: 10%;
+  transform: translateY(-60%);
 }
 
-
-
-
+ul {
+  text-align: center;
+}
+.img-profil {
+  display: flex;
+  flex-direction: column;
+  border-radius: 50%;
+}
 
 .pointer {
   cursor: pointer;
@@ -185,9 +174,6 @@ ul{
       supported by Chrome, Edge, Opera and Firefox */
 }
 
-
-
-
 .columns {
   width: 100%;
   display: grid;
@@ -196,14 +182,11 @@ ul{
   grid-column-gap: 16px;
 }
 
-li{
-    list-style-type: none;
-    border: 2px solid rgb(37, 115, 225);
-    padding: 40px 300px;
-    margin: 5px;
-    border-radius: 5px;
-    
+li {
+  list-style-type: none;
+  border: 4px solid rgb(37, 115, 225);
+  padding: 40px 300px;
+  margin: 5px;
+  border-radius: 5px;
 }
-
-
 </style>
